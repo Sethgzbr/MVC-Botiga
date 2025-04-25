3495830bbfbed2f0128eaafe31e246a22c83bda8
Isma <2023_Ismael.younes@iticbcn.cat>
2025-04-25 11:02:37 +0200

    Ex 5 --> DTOs acabat

--- botiga/botiga/src/main/java/com/example/botiga/DTO/CategoriaDTO.java
+++ botiga/botiga/src/main/java/com/example/botiga/DTO/CategoriaDTO.java
@@ -1,8 +1,32 @@
 package com.example.botiga.DTO;
 
-import com.example.botiga.Model.Subcategoria;
 public class CategoriaDTO {
     private String desc_Categoria;
     private String status_Categoria;
-    private Subcategoria subcategoria;
+    private SubcategoriaDTO subcategoria;
+
+    // Getters y Setters
+    public String getDesc_Categoria() {
+        return desc_Categoria;
+    }
+
+    public void setDesc_Categoria(String desc_Categoria) {
+        this.desc_Categoria = desc_Categoria;
+    }
+
+    public String getStatus_Categoria() {
+        return status_Categoria;
+    }
+
+    public void setStatus_Categoria(String status_Categoria) {
+        this.status_Categoria = status_Categoria;
+    }
+
+    public SubcategoriaDTO getSubcategoria() {
+        return subcategoria;
+    }
+
+    public void setSubcategoria(SubcategoriaDTO subcategoria) {
+        this.subcategoria = subcategoria;
+    }
 }
--- botiga/botiga/src/main/java/com/example/botiga/DTO/SubcategoriaDTO.java
+++ botiga/botiga/src/main/java/com/example/botiga/DTO/SubcategoriaDTO.java
@@ -6,5 +6,31 @@ public class SubcategoriaDTO {
 
     private String desc_Subcategoria;
     private String status_Subcategoria;
-    Categoria categoria;
+    private CategoriaDTO categoria;
+
+    // Getters Setters
+    public String getDesc_Subcategoria() {
+        return desc_Subcategoria;
+    }
+
+    public void setDesc_Subcategoria(String desc_Subcategoria) {
+        this.desc_Subcategoria = desc_Subcategoria;
+    }
+
+    public String getStatus_Subcategoria() {
+        return status_Subcategoria;
+    }
+
+    public void setStatus_Subcategoria(String status_Subcategoria) {
+        this.status_Subcategoria = status_Subcategoria;
+    }
+
+    public CategoriaDTO getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(CategoriaDTO categoria) {
+        this.categoria = categoria;
+    }
+
 }
